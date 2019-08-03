07b38c278695b9209d38ddc5500b6b8f0b6b1f45
John Brick <john.n.brick@gmail.com>
2019-08-02 21:48:04 -0600

    Sad attempt at dynamics

--- attitude_controller/AttitudeController.m
+++ attitude_controller/AttitudeController.m
@@ -18,13 +18,34 @@ I_tru=I_est;
 
 % Scenario Definition
 Q0=[0 0 0 1];
-w0=[0 0 0];
+w0=[0 .1 .2];
 
 t0=0;
 tf=100;
 
-
-
-% 
+% principal axes and MOIs
 [princ_axes_tru,princ_moms_tru]=eig(I_tru);
+princ_moms_tru=[princ_moms_tru(1,1) princ_moms_tru(2,2) princ_moms_tru(3,3)]';
 
+dyn=@(t,x) attitude_dynamics(x,t,princ_moms_tru,[0 0 0]');
+X0=[Q0,w0];
+
+options=odeset('AbsTol',1e-12,'RelTol',1e-12);
+[T,X]=ode45(dyn,[t0,tf],X0,options);
+
+W=X(:,5:7)
+
+for i=1:size(X,1)
+	H(i)=norm(W(i,:).*princ_moms_tru);
+	T(i)=1/2*W(i,:)*W(i,:)';
+	end
+	
+
+figure()
+subplot(3,1,1)
+hold on
+plot(W)
+subplot(3,1,2)
+plot(H)
+subplot(3,1,3)
+plot(T)
