de8706c509f519eca79f820c8c3bf18831777f08
John Brick <john.n.brick@gmail.com>
2019-08-04 20:07:00 -0600

    git weirdness

---
+++ attitude_controller/attitude_dynamics2.m
@@ -0,0 +1,28 @@
+function xdot=attitude_dynamics(x,t,I,T)
+	%attitude dynamics about principal axes
+	%keyboard
+	%state vector is 7 long
+	%x=reshape(x,[7,1]);
+	
+	q_ijk=[x(1);x(2);x(3)];
+	q_0=x(4);
+	w=[x(5);x(6);x(7)];
+	
+	%	3 vec components of quaternion (x,y,z)
+	xdot(1:3)=1/2*(skew(q_ijk)+q_0*eye(3))*w;
+	%	1 scalar component of quaternion
+	xdot(4)=-1/2*q_ijk'*w;
+	%	3 vec components of body rate
+<<<<<<< HEAD
+	xdot(5)=(I(2,2)-I(3,3))/I(1,1)*w(2)*w(3)+T(1)/I(1,1);
+	xdot(6)=(I(3,3)-I(1,1))/I(2,2)*w(3)*w(1)+T(2)/I(2,2);
+	xdot(7)=(I(1,1)-I(2,2))/I(3,3)*w(1)*w(2)+T(3)/I(3,3);
+	xdot=xdot';
+=======
+	xdot(5)=(I(2)-I(3))/I(1)*w(2)*w(3)+T(1)/I(1);
+	xdot(6)=(I(3)-I(1))/I(2)*w(3)*w(1)+T(2)/I(2);
+	xdot(7)=(I(1)-I(2))/I(3)*w(1)*w(2)+T(3)/I(3);
+	xdot = xdot';
+>>>>>>> 7f783926f1d49f5a802249fcbdddc4fc326894ba
+
+end
