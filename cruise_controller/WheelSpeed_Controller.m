5ab8c6ca3e08c366cb366e3c39c4218a5880c0cd
John Brick <john.n.brick@gmail.com>
2019-08-06 20:22:13 -0600

    Finished KFilter

--- cruise_controller/WheelSpeed_Controller.m
+++ cruise_controller/WheelSpeed_Controller.m
@@ -5,31 +5,54 @@ close all
 
 C_F=.1;
 
-dt=.1;
+dt=1;
 
 t=[0:dt:100];
 
 STM=exp(-C_F*dt);
 
-Q=[.1];
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 X0=[500];
 X(1)=X0;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%	Propagate Truth
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Q_truth=[10];%process
+R_truth=[30];%measurement
+
+Q=10;
+R=30;
+
+H=1;
+
+X0_est=400;
+P0=10000;
+
+Xest_Array(1)=X0_est;
+P=P0;
+P_array(1)=P;
 
 for i=2:length(t)
-	X(i)=STM*X(i-1)+randn(1)*sqrt(Q);
+	X(i)=STM*X(i-1)+randn(1)*sqrt(Q_truth);%truth
+	Z(i)=X(i)+randn(1)*sqrt(R_truth); %measurement
+	P=((STM*P*STM'+Q)^-1+H'*R^-1*H)^-1; %covariance update (propagation and measurement)
+	K=P*H'*R^-1; %Gain Matrix
+	Xest_Array(:,i)=STM*Xest_Array(:,i-1)+K*(Z(:,i)-H*(STM*Xest_Array(:,i-1))); %State Estimate
+	P_array(i)=P;
 end
-	
+
+
+figure()
+hold on
+	plot(X)
+	plot(Z,'.')
+	plot(Xest_Array)
 	
 figure()
-	plot(X)
-	
-	
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%	Propagate Truth
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+hold on
+plot(P_array)
+
+
+
+
+
+
